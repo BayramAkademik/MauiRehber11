e4150f2fe6e61676106ce84c7fe1d035d73f1840
Bayram <Bayram@DESKTOP-BNKS5SI>
2023-12-20 23:53:31 +0300

    Proje dosyası ekle.

---
+++ MauiAppHafta11/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppHafta11
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppHafta11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppHafta11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
